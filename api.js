3567694f66f258421809f2bd07a3cd4f2f0add9e
maksrover <maksrover@gmail.com>
2023-10-14 14:21:44 +0300

    final test

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const personalKey = "sautner-denis";
+const personalKey = "atamyrat-isayev";
 const defaultKey = "prod"
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${defaultKey}/instapro`;
@@ -95,7 +95,7 @@ export const removeLike = ({ token, postId }) => {
     })
 }
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
@@ -128,7 +128,7 @@ export function loginUser({ login, password }) {
   });
 }
 
-// Загружает картинку в облако, возвращает url загруженной картинки
+
 export function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
