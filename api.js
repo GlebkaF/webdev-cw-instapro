0ca0181178915c1013371d619717caf4d75b959f
maksrover <maksrover@gmail.com>
2023-10-15 10:07:47 +0300

    api

--- api.js
+++ api.js
@@ -1,8 +1,7 @@
-// Замени на свой, чтобы получить независимый от других набор данных.
-// "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKey = "atamyrat-isayev";
+const defaultKey = "prod"
 const baseHost = "https://webdev-hw-api.vercel.app";
-const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
+const postsHost = `${baseHost}/api/v1/${defaultKey}/instapro`;
 
 export function getPosts({ token }) {
   return fetch(postsHost, {
@@ -22,6 +21,80 @@ export function getPosts({ token }) {
       return data.posts;
     });
 }
+
+export function getUserPosts({ token, userId }) {
+  return fetch(postsHost + `/user-posts/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+export const addPost = ({ token, description, imageUrl }) => {
+  return fetch(postsHost, {
+    method: "POST",
+    body: JSON.stringify({
+      description,
+      imageUrl,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 400) {
+        alert("Выберите фото и добавьте комментарий");
+        throw new Error("Выберите фото и добавьте комментарий");
+      }
+
+      return response.json();
+    })
+}
+
+export const setLike = ({ token, postId }) => {
+  return fetch(postsHost + '/' + postId + "/like", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        alert('Лайкать посты могут только авторизованные пользователи');
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+}
+
+export const removeLike = ({ token, postId }) => {
+  return fetch(postsHost + '/' + postId + "/dislike", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+}
+
 
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
