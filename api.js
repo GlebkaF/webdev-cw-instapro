a839692a3cce5d2d8c53a7b8c7e59b3585074110
EkaFord <138514574+EkaFord@users.noreply.github.com>
2023-10-22 11:39:04 -0400

    fisrt batch components

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+
+import { getToken } from "./index.js";
+
+const personalKey = "ekaford";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
@@ -23,7 +26,26 @@ export function getPosts({ token }) {
     });
 }
 
+
+export function getUserPosts({ token, userId }) {
+  return fetch(postsHost + "/user-posts/" + userId, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+
+//registration
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
@@ -41,6 +63,7 @@ export function registerUser({ login, password, name, imageUrl }) {
   });
 }
 
+//authorization
 export function loginUser({ login, password }) {
   return fetch(baseHost + "/api/user/login", {
     method: "POST",
@@ -67,4 +90,66 @@ export function uploadImage({ file }) {
   }).then((response) => {
     return response.json();
   });
+}
+
+//upload post on server
+
+export function sendPost({ description, imageUrl }) {
+  return fetch("https://wedev-api.sky.pro/api/v1/ekaford/instapro", {
+    method: "POST",
+    body: JSON.stringify({
+      description: description
+      .replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll('"', "&quot;"),
+      imageUrl: imageUrl
+    }),
+    headers: {
+      Authorization: getToken(),
+    }
+  }).then((response) => {
+    return response.json();
+  }).then(() => {
+    console.log("отправлено");
+  })
+}
+
+//like
+
+export function postLike({ id }) {
+  return fetch(postsHost + "/" + id + "/like", {
+    method: "POST",
+    headers: {
+      Authorization: getToken(),
+    }
+  }).then((response) => {
+    return response.json();
+  })
+}
+
+//delete like
+
+export function postDisLike({ id }) {
+  return fetch(postsHost + "/" + id + "/dislike", {
+    method: "POST",
+    headers: {
+      Authorization: getToken(),
+    }
+  }).then((response) => {
+    return response.json();
+  })
+}
+
+//delete post
+
+export function deletePost({ id }) {
+  return fetch(postsHost + "/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: getToken(),
+    }
+  }).then((response) => {
+    return response.json();
+  })
 }
