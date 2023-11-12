447ae3b5793a609843ed322c3c2d8c48f3683ef4
DeliriousEC <m-trankov@mail.ru>
2023-11-12 23:25:22 +0300

    Курсач +- готовый

--- components/header-component.js
+++ components/header-component.js
@@ -1,24 +1,22 @@
-import { goToPage, logout, user } from "../index.js";
+import { goToPage, logout, user } from "../main.js";
 import { ADD_POSTS_PAGE, AUTH_PAGE, POSTS_PAGE } from "../routes.js";
 
 export function renderHeaderComponent({ element }) {
   element.innerHTML = `
-  <div class="page-header">
-      <h1 class="logo">instapro</h1>
-      <button class="header-button add-or-login-button">
-      ${
-        user
-          ? `<div title="Добавить пост" class="add-post-sign"></div>`
-          : "Войти"
-      }
-      </button>
-      ${
-        user
-          ? `<button title="${user.name}" class="header-button logout-button">Выйти</button>`
-          : ""
-      }  
-  </div>
-  
+<div class="page-header">
+<h1 class="logo">instapro</h1>
+<button class="header-button add-or-login-button">
+${user
+      ? `<div title="Добавить пост" class="add-post-sign"></div>`
+      : "Войти"
+    }
+</button>
+${user
+      ? `<button title="${user.name}" class="header-button logout-button">Выйти</button>`
+      : ""
+    }
+</div>
+
 `;
 
   element
