42733c145d4173fa369020a1f7c89740227ce1c7
EkaterinaMikhalko <mikhalkoea@gmail.com>
2024-01-11 01:42:45 +0300

    added posts from api

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -4,13 +4,45 @@ import { posts, goToPage } from "../index.js";
 
 export function renderPostsPageComponent({ appEl }) {
   // TODO: реализовать рендер постов из api
+  const appHtml = posts.map((post) => {
+    return ` 
+              <div class="page-container">
+                <div class="header-container"></div>
+                <ul class="posts">
+                  <li class="post">
+                    <div class="post-header" data-user-id="${post.user.id}">
+                        <img src="${post.user.imageUrl}" class="post-header__user-image">
+                        <p class="post-header__user-name">${post.user.name}</p>
+                    </div>
+                    <div class="post-image-container">
+                      <img class="post-image" src="${post.user.imageUrl}">
+                    </div>
+                    <div class="post-likes">
+                      <button data-post-id="${post.id}" class="like-button">
+                        <img src="./assets/images/like-active.svg">
+                      </button>
+                      <p class="post-likes-text">
+                        Нравится: <strong>2</strong>
+                      </p>
+                    </div>
+                    <p class="post-text">
+                      <span class="user-name">${post.user.name}</span>
+                      ${post.text}
+                    </p>
+                    <p class="post-date">
+                      19 минут назад
+                    </p>
+                  </li>
+                  </ul>
+              </div>`;
+  });
   console.log("Актуальный список постов:", posts);
 
   /**
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
-  const appHtml = `
+ /* const appHtml = `
               <div class="page-container">
                 <div class="header-container"></div>
                 <ul class="posts">
@@ -91,7 +123,7 @@ export function renderPostsPageComponent({ appEl }) {
                     </p>
                   </li>
                 </ul>
-              </div>`;
+              </div>`;*/
 
   appEl.innerHTML = appHtml;
 
