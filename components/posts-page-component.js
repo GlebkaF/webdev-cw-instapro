521333220da8bcdf4a3264113ed8c47f484000ee
EkaterinaMikhalko <mikhalkoea@gmail.com>
2024-01-13 01:13:28 +0300

    Added add-post-page

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -1,17 +1,21 @@
 import { USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
 import { posts, goToPage } from "../index.js";
+//import { formatDistanceToNow } from "../node_modules/date-fns";
 
 export function renderPostsPageComponent({ appEl }) {
   // TODO: реализовать рендер постов из api
   const appHtml = posts.map((post) => {
+    //const postDateBefore = formatDistanceToNow(new Date(post.createdAt));
     return ` 
               <div class="page-container">
                 <div class="header-container"></div>
                 <ul class="posts">
                   <li class="post">
                     <div class="post-header" data-user-id="${post.user.id}">
-                        <img src="${post.user.imageUrl}" class="post-header__user-image">
+                        <img src="${
+                          post.user.imageUrl
+                        }" class="post-header__user-image">
                         <p class="post-header__user-name">${post.user.name}</p>
                     </div>
                     <div class="post-image-container">
@@ -27,10 +31,10 @@ export function renderPostsPageComponent({ appEl }) {
                     </div>
                     <p class="post-text">
                       <span class="user-name">${post.user.name}</span>
-                      ${post.text}
+                      ${post.description}
                     </p>
                     <p class="post-date">
-                      19 минут назад
+                     
                     </p>
                   </li>
                   </ul>
@@ -42,7 +46,7 @@ export function renderPostsPageComponent({ appEl }) {
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
- /* const appHtml = `
+  /* const appHtml = `
               <div class="page-container">
                 <div class="header-container"></div>
                 <ul class="posts">
