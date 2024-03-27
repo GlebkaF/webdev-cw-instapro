a7bad6791525ecd9c1b67acbb582bb300f3b8b8f
ArtemKhoroshavin <horoshavin2012@yande.ru>
2024-03-27 23:51:35 +0300

    mmm

--- api.js
+++ api.js
@@ -1,194 +1,148 @@
-import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
-import { getToken, posts, setPosts } from "./index.js";
-
-
-
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "artem-horoshavin";
-const baseHost = "https://webdev-hw-api.vercel.app";
-const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
+const personalKey = 'artem-horoshavin'
+const baseHost = 'https://wedev-api.sky.pro'
 
+const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`
 
 export function getPosts({ token }) {
-  return fetch(postsHost, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-
-      if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
-      return response.json();
+    return fetch(postsHost, {
+        method: 'GET',
+        headers: {
+            Authorization: token,
+        },
     })
-    .then((data) => {
-      console.log(data.posts)
-      return data.posts.map((post) => {
-        return {
-          id: post.id,
-          userId: post.user.id,
-          imageUrl: post.imageUrl,
-          date: post.createdAt,
-          description: post.description,
-          login: post.user.login,
-          likes: post.likes,
-          imageUrlAvatar: post.user.imageUrl,
-          name: post.user.name,
-          isLiked: post.isLiked,
-          created: post.createdAt
-        }
+        .then((response) => {
+            if (response.status === 401) {
+                throw new Error('Нет авторизации')
+            }
 
-      })
+            return response.json()
+        })
+        .then((data) => {
+            return data.posts
+        })
+}
 
+export function postPosts({ token, description, imageUrl }) {
+    return fetch(postsHost, {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+        body: JSON.stringify({
+            imageUrl: imageUrl,
+            description: description
+                .replaceAll('&', '&amp;')
+                .replaceAll('<', '&lt;')
+                .replaceAll('>', '&gt;')
+                .replaceAll('"', '&quot;'),
+        }),
     })
+        .then((response) => {
+            if (response.status === 201) {
+                return response.json()
+            } else if (response.status === 400) {
+                throw new Error(
+                    'Something wrong. Проверьте данные и попробуйте снова.'
+                )
+            } else {
+                throw new Error(
+                    'Произошла ошибка при загрузке поста. Пожалуйста, попробуйте позже.'
+                )
+            }
+        })
+        .catch((error) => {
+            alert('Произошла ошибка:', error)
+            throw error
+        })
 
+    //ДОБАВИТЬ ПРОВЕРКУ CATCH ERROR THEN + если файл больше 5 мб
 }
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
 export function registerUser({ login, password, name, imageUrl }) {
-  return fetch(baseHost + "/api/user", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-      name,
-      imageUrl,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Такой пользователь уже существует");
-    }
-    return response.json();
-  });
+    return fetch(baseHost + '/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            login,
+            password,
+            name,
+            imageUrl,
+        }),
+    }).then((response) => {
+        if (response.status === 400) {
+            throw new Error('Такой пользователь уже существует')
+        }
+        return response.json()
+    })
 }
 
 export function loginUser({ login, password }) {
-  return fetch(baseHost + "/api/user/login", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Неверный логин или пароль");
-    }
-    return response.json();
-  });
-}
-
-// Загружает картинку в облако, возвращает url загруженной картинки нужно чтобы через возвращаемую ссылку получить доступ к фото?
-export function uploadImage({ file }) {
-  const data = new FormData();
-  data.append("file", file);
-
-  return fetch(baseHost + "/api/upload/image", {
-    method: "POST",
-    body: data,
-  }).then((response) => {
-    return response.json();
-  });
-}
-
-
-export function posting({ token, description, imageUrl }) {
-  return fetch(postsHost, {
-    method: "POST",
-    headers: {
-      Authorization: token
-    },
-    body: JSON.stringify({
-      description,
-      imageUrl,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("что то не так");
-    }
-    return response.json();
-  });
-}
-
-export function getUserPosts(token, userId) {
-  return fetch(postsHost + `/user-posts/${userId}`, {
-    headers: {
-      Authorization: token
-    },
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data.posts)
-      return data.posts.map((post) => {
-        return {
-          id: post.id,
-          imageUrl: post.imageUrl,
-          date: post.createdAt,
-          description: post.description,
-          login: post.user.login,
-          isLiked: post.isLiked,
-          userId: post.user.id,
-          likes: post.likes,
-          imageUrlAvatar: post.user.imageUrl,
-          name: post.user.name,
-          created: post.createdAt
+    return fetch(baseHost + '/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            login,
+            password,
+        }),
+    }).then((response) => {
+        if (response.status === 400) {
+            throw new Error('Неверный логин или пароль')
         }
-
-      })
-    })
-
-}
-
-
-
-export function addLike({ token, id }) {
-  return fetch(`${postsHost}/${id}/like`, {
-    method: "POST",
-    headers: {
-      Authorization: token
-    },
-  }).then((response) => {
-    if (response.status === 401) {
-      throw new Error("нет авторизации");
-    }
-    return response.json()
-  })
-    .catch((error) => {
-      if (error.message === "нет авторизации") {
-        alert("Нет авторизации");
-      }
+        return response.json()
     })
 }
 
+// Загружает картинку в облако, возвращает url загруженной картинки
+export function uploadImage({ file }) {
+    const data = new FormData()
+    data.append('file', file)
 
-export function disLike({ token, id }) {
-  return fetch(`${postsHost}/${id}/dislike`, {
-    method: "POST",
-    headers: {
-      Authorization: token
-    },
-  }).then((response) => {
-    if (response.status === 401) {
-      throw new Error("нет авторизации");
-    }
-    return response.json();
-  });
+    return fetch(baseHost + '/api/upload/image', {
+        method: 'POST',
+        body: data,
+    }).then((response) => {
+        return response.json()
+    })
 }
 
-export function deletePostFetch({ token, id }) {
-  return fetch(`${postsHost}/${id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: token
-    },
-  })
-    .then((response) => {
-      return response.json();
+// Function to active Like!
+
+export function likePost({ id, token, name }) {
+    return fetch(postsHost + `/${id}/like`, {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+        body: JSON.stringify({
+            id,
+            name,
+        }),
+    }).then((response) => {
+        return response.json
     })
+}
 
+// Function to not active Like!
 
+export function dislikePost({ id, token }) {
+    return fetch(postsHost + `/${id}/dislike`, {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+    }).then((response) => {
+        return response.json
+    })
+}
+
+export function getUserPost({ userId }) {
+    console.log(userId)
+    return fetch(postsHost + `/user-posts/${userId}`, {
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            return data.posts
+        })
 }
--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -1,161 +1,129 @@
-import { POSTS_PAGE, USER_POSTS_PAGE } from "../routes.js";
-import { renderHeaderComponent } from "./header-component.js";
-import { posts, goToPage, getToken, renderApp, setPosts } from "../index.js";
-import { addLike, disLike, getPosts } from "../api.js";
+import { USER_POSTS_PAGE } from '../routes.js'
+import { renderHeaderComponent } from './header-component.js'
+import { posts, goToPage, getToken, setPosts } from '../index.js'
+import { dislikePost, getPosts, likePost } from '../api.js'
+
 import { formatDistanceToNow } from "date-fns";
 import { ru } from "date-fns/locale";
 
 export function renderPostsPageComponent({ appEl }) {
-  console.log("Актуальный список постов:", posts);
+    // TODO: реализовать рендер постов из api
 
+    const appElement = document.getElementById('app')
 
-  /**
-   * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
-   * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
-   */
+    // получение разметки в html из api
 
+    const render = () => {
+        const postsHtml = posts
+            .map((post) => {
 
+                const createDate = formatDistanceToNow(new Date(post.createdAt), { addSuffix: true, locale: ru })
 
-  const appHtml = posts.map((post, index) => {
+                return `<li class="post">
+                    <div class="post-header" data-user-id="${post.user.id}">
+                        <img src="${
+                            post.user.imageUrl
+                        }" class="post-header__user-image">
+                        <p class="post-header__user-name">${post.user.login}</p>
+                    </div>
 
-    console.log("имя последнего лайкнувшего", post.likes[post.likes.length - 1]?.name)
+                    <div class="post-image-container">
+                      <img class="post-image" src="${post.imageUrl}">
+                    </div>
 
-    return `
+                    <div class="post-likes" >
+                      <button data-post-id="${post.id}" data-dislike="${
+                          post.isLiked ? 'true' : ''
+                      }" class="like-button">
+                        <img src="${
+                            post.isLiked
+                                ? `./assets/images/like-active.svg`
+                                : `./assets/images/like-not-active.svg`
+                        }">
 
-      <li class="post">
-        <div class="post-header" data-user-id=${post.userId}>
-            <img src=${post.imageUrlAvatar} class="post-header__user-image">
-            <p class="post-header__user-name">${post.name}</p>
-         
-        </div>
-        <div class="post-image-container">
-       
-          <img class="post-image" src=${post.imageUrl}>
-        </div>
-    <div class="container-for-elements">
-        <div class="post-likes">
+                      </button>
+                      <p class="post-likes-text">
+                         <strong>${
+                             post.likes.length >= 1
+                                 ? `Нравится: ${post.likes[0]?.name}`
+                                 : ' '
+                         }
+                         <strong>${
+                             post.likes.length > 1
+                                 ? `и еще ${post.likes.length - 1}`
+                                 : ' '
+                         }</strong>
+                      </p>
+                    </div>
 
-        <button data-id=${post.id} data-index=${index} class="like-button">
-        
-        <img src= ${post.isLiked ? `./assets/images/like-active.svg>` : `./assets/images/like-not-active.svg>`}
-        </button>
+                    <p class="post-text">
+                      <span class="user-name">${post.user.name}</span>
+                      ${post.description}
+                    </p>
+                    
+                    <p class="post-date">
+                      ${createDate}
+                    </p>
+                  </li>`
+            })
+            .join(' ')
 
-        <p class="post-likes-text">
-          Нравится: <strong> ${post.likes.length > 1 ? `${post.likes[post.likes.length - 1]?.name} и еще ${post.likes.length - 1}`
-        : post.likes.length === 0 ? "никто не лайкнул" :
-          post.likes[0].name}</strong> 
-          </p>
-        
-      </div>
-      <div class="delete-element">
-      <button class="delete-btn"  data-id=${post.id}> Удалить </button>
-      </div>
-  </div>
-        <p class="post-text">
-          <span class="user-name">${post.name}</span>
-          ${post.description}
-        </p>
-        <p class="post-date">
-        ${formatDistanceToNow(post.created, new Date(),{
-          locale: ru,
-        })}
-        </p>
-   
-      </li>`}).join("")
+        const appHtml = `
+                <div class="page-container">
 
+                  <div class="header-container"></div>
 
+                  <ul class="posts">
+                    ${postsHtml}
+                  </ul>
 
+                </div>`
 
-  appEl.innerHTML = `<div class="page-container">
-  <div class="header-container"></div>
-  <ul class="posts">${appHtml}</ul> 
-  </div>`;
+        appEl.innerHTML = appHtml
 
+        renderHeaderComponent({
+            element: document.querySelector('.header-container'),
+        })
 
-
-  renderHeaderComponent({
-    element: document.querySelector(".header-container"),
-  });
-
-  for (let userEl of document.querySelectorAll(".post-header")) {
-    userEl.addEventListener("click", () => {
-      goToPage(USER_POSTS_PAGE, {
-        userId: userEl.dataset.userId,
-      });
-
-    });
-  }
-
-
-  function makeLike() {
-
-
-    const likesButton = document.querySelectorAll(".like-button");
-
-    for (const likeButton of likesButton) {
-
-      likeButton.addEventListener("click", (event) => {
-        event.stopPropagation();
-        if (!getToken()) {
-          alert("Нужно авторизоваться")
-          return
-        }
-        const id = likeButton.dataset.id;
-        console.log("id поста:", id);
-        const index = likeButton.dataset.index;
-        console.log("индекс чела:", index);
-
-
-        if (posts[index].isLiked === true) {
-          return disLike({ token: getToken(), id })
-            .then(() => {
-              getPosts({ token: getToken() })
-                .then((response) => {
-                  setPosts(response)
-                  renderApp();
+        for (let userEl of document.querySelectorAll('.post-header')) {
+            userEl.addEventListener('click', () => {
+                goToPage(USER_POSTS_PAGE, {
+                    userId: userEl.dataset.userId,
                 })
             })
         }
-        else {
 
-          return addLike({ token: getToken(), id })
-            .then(() => {
-              getPosts({ token: getToken() })
-                .then((response) => {
-                  setPosts(response)
-                  renderApp();
-                })
+        for (let likeButton of document.querySelectorAll('.like-button')) {
+            likeButton.addEventListener('click', () => {
+                // console.log(likeButton.dataset.postId)
+                // console.log(likeButton.dataset.dislike)
 
+                if (likeButton.dataset.dislike) {
+                    dislikePost({
+                        id: likeButton.dataset.postId,
+                        token: getToken(),
+                    }).then(() => {
+                        getPosts({ token: getToken() }).then((newPosts) => {
+                            console.log(newPosts)
+                            setPosts(newPosts)
+                            renderPostsPageComponent({ appEl })
+                        })
+                    })
+                } else {
+                    likePost({
+                        id: likeButton.dataset.postId,
+                        token: getToken(),
+                    }).then(() => {
+                        getPosts({ token: getToken() }).then((newPosts) => {
+                            console.log(newPosts)
+                            setPosts(newPosts)
+                            renderPostsPageComponent({ appEl })
+                        })
+                    })
+                }
             })
         }
-
-      })
-
     }
 
-  }
-  makeLike()
-
-
-  // const deleteItems = document.querySelectorAll('.delete-btn')
-  // for (const deleteItem of deleteItems) {
-  //   deleteItem.addEventListener("click", () => {
-  //     if (!getToken()) {
-  //       alert("Нужно авторизоваться")
-  //       return
-  //     }
-  //     const id = deleteItem.dataset.id;
-  //     console.log(id)
-  //     deletePostFetch({ token: getToken(), id })
-  //       .then(() => {
-  //         alert("Удалил пост"),
-  //           goToPage(POSTS_PAGE)
-
-  //       })
-
-  //   })
-  // }
-
-
-
+    render()
 }
